b06ce2996b50f65803d0883316b16ba3fc797717
Julio Lopez <julioj.lopeza@gmail.com>
2019-04-17 23:23:43 -0400

    Action Types

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+const INCREMENTAR = 'CONTADOR/INCREMENTAR'
+const DECREMENTAR = 'CONTADOR/DECREMENTAR'
+
+
+
+const initialState = 0
+
+
+
+export default function (state = initialState, action){
+    console.log(action)
+    switch (action.type){
+        case INCREMENTAR:
+            return state + 1
+
+        case DECREMENTAR:
+            return state-1
+        default
+            return state
+
+    }
+    
+}
